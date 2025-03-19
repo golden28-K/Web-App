725117deedea70f42e138d5d3d065eae856eecea
golden28-K <70968123+golden28-K@users.noreply.github.com>
2025-03-19 11:06:33 +0200

    Changes made to the authentication in the serve JS file

--- Back End/index.js
+++ Back End/index.js
@@ -1,20 +1,19 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/ReviewsDAO.js"
+//import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
 const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.x00rp.mongodb.net/`
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.x00rp.mongodb.net/MOVIE REVIEWS?authsource=admin`;
 
 const port = 8000
-
+ 
 MongoClient.connect (
     uri,
     {
         maxPoolSize: 50,
         wtimeoutMS: 2500,
-        useNewUrlParser: true
     })
     .catch(err => {
         console.error(err.stack)
